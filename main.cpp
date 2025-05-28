85422efcd7e656e71ef7e1d97ea48e13653eebe8
Yuya-Kojima <k024g0104@g.neec.ac.jp>
2025-05-28 16:55:22 +0900

    multiply関数のm[0][3]が間違っていた
    今までx軸方向の移動をしてから回転をしていなかった可能性があり、気づかなかったと思う

--- main.cpp
+++ main.cpp
@@ -265,7 +265,7 @@ Matrix4x4 Multiply(Matrix4x4 matrix1, Matrix4x4 matrix2) {
 
   result.m[0][3] =
       matrix1.m[0][0] * matrix2.m[0][3] + matrix1.m[0][1] * matrix2.m[1][3] +
-      matrix1.m[0][2] * matrix2.m[2][3] + matrix1.m[3][0] * matrix2.m[3][3];
+      matrix1.m[0][2] * matrix2.m[2][3] + matrix1.m[0][3] * matrix2.m[3][3];
 
   result.m[1][0] =
       matrix1.m[1][0] * matrix2.m[0][0] + matrix1.m[1][1] * matrix2.m[1][0] +
@@ -501,13 +501,14 @@ Matrix4x4 MakeAffineMatrix(Vector3 scale, Vector3 rotate, Vector3 translate) {
 Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio,
                                    float nearClip, float farClip) {
   Matrix4x4 matrix;
+  float f = 1.0f / tanf(fovY / 2.0f);
 
-  matrix.m[0][0] = 1.0f / aspectRatio * (cosf(fovY / 2.0f) / sinf(fovY / 2.0f));
+  matrix.m[0][0] = f / aspectRatio;
   matrix.m[0][1] = 0.0f;
   matrix.m[0][2] = 0.0f;
   matrix.m[0][3] = 0.0f;
   matrix.m[1][0] = 0.0f;
-  matrix.m[1][1] = cosf(fovY / 2.0f) / sinf(fovY / 2.0f);
+  matrix.m[1][1] = f;
   matrix.m[1][2] = 0.0f;
   matrix.m[1][3] = 0.0f;
   matrix.m[2][0] = 0.0f;
@@ -516,7 +517,7 @@ Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio,
   matrix.m[2][3] = 1.0f;
   matrix.m[3][0] = 0.0f;
   matrix.m[3][1] = 0.0f;
-  matrix.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
+  matrix.m[3][2] = -nearClip * farClip / (farClip - nearClip);
   matrix.m[3][3] = 0.0f;
 
   return matrix;
