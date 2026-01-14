9d87803f1c705dea0542323e5e52524954bd89ac
Yuya_Kojima <hamutahamuchi@gmail.com>
2026-01-14 09:53:25 +0900

    main.cppの不要なインクルードを削除

--- project/Application/main.cpp
+++ project/Application/main.cpp
@@ -1,35 +1,5 @@
-#include "DirectXTex.h"
-#include "Math/MathUtil.h"
-#include "Math/Matrix4x4.h"
-#include "Math/Transform.h"
-#include "Math/Vector4.h"
-#include "Math/VertexData.h"
-// #include "externals/imgui/imgui.h"
-// #include "externals/imgui/imgui_impl_dx12.h"
-// #include "externals/imgui/imgui_impl_win32.h"
-#include "imgui.h"
-// #include <Windows.h>
-
-#include <cstdint>
-#include <d3d12.h>
-#include <dxcapi.h>
-#include <dxgi1_6.h>
-#include <dxgidebug.h>
-#include <format>
-#include <string>
-#define _USE_MATH_DEFINES
+#include "Core/EngineBase.h"
 #include "Core/Game.h"
-
-#include "Util/StringUtil.h"
-#include <dinput.h>
-#include <math.h>
-#include <numbers>
-#include <random>
-
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd,
-                                                             UINT msg,
-                                                             WPARAM wParam,
-                                                             LPARAM lParam);
 
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
@@ -39,8 +9,6 @@ extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd,
 #pragma comment(lib, "dinput8.lib")
 #pragma comment(lib, "dxguid.lib")
 
-#include "Core/EngineBase.h"
-
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -48,15 +16,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   EngineBase *game = new Game();
 
   game->Run();
-
-  // 実際のcommandListのImGuiの描画コマンドを積む
-  // ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(),
-  //                              dx12Core->GetCommandList());
-
-  // ImGuiの終了処理
-  // ImGui_ImplDX12_Shutdown();
-  // ImGui_ImplWin32_Shutdown();
-  // ImGui::DestroyContext();
 
   delete game;
 
