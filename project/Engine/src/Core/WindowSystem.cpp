6242f0d9af91fb86d72de25c230c631e344f077d
Yuya_Kojima <hamutahamuchi@gmail.com>
2026-02-04 14:22:55 +0900

    タイトルバー変更

--- project/Engine/src/Core/WindowSystem.cpp
+++ project/Engine/src/Core/WindowSystem.cpp
@@ -64,7 +64,7 @@ void WindowSystem::Initialize() {
   hwnd = CreateWindow(
 
       wc.lpszClassName,     // 利用するクラス名
-      L"CG2",               // タイトルバーの文字
+      L"2324_パスガイド",   // タイトルバーの文字
       WS_OVERLAPPEDWINDOW,  // よく見るウィンドウスタイル
       CW_USEDEFAULT,        // 表示X座標
       CW_USEDEFAULT,        // 表示Y座標
