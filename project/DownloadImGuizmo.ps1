8ebcd0db37b33a118ed29fb042742587f0843df9
Yuya_Kojima <hamutahamuchi@gmail.com>
2026-07-29 15:18:06 +0900

    ポストエフェクトがテストコードの選択式のままだったので、複数のポストエフェクトを掛けられるように修正 ディゾルブのデフォルトノイズ画像を設定

--- project/DownloadImGuizmo.ps1
+++
@@ -1,2 +0,0 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/CedricGuillemet/ImGuizmo/master/src/ImGuizmo.h" -OutFile "externals\imgui\ImGuizmo.h"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/CedricGuillemet/ImGuizmo/master/src/ImGuizmo.cpp" -OutFile "externals\imgui\ImGuizmo.cpp"
